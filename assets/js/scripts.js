5038673fba07b2e4f623632561d4ec1408cd3737
wesselstoker <wessel.stoker@hotmail.com>
2014-03-09 21:17:15 +0100

    Random color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    $('.intro').slideDown(1500);
+    var colors = new Array("#3498db","#2ecc71","#c0392b","#34495e","#e67e22","#27ae60","#2c3e50","#c0392b");
+    var currentColor = colors[Math.floor(Math.random() * colors.length)];
+
+    $('.intro').css("background-color", currentColor).slideDown(1500);
 });
